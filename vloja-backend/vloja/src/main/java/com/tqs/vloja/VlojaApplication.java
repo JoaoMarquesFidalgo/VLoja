7851a407f6c66df3386a99a207c338a6c5a0777c
fidalgo <jfidalg@gmail.com>
2019-08-30 03:48:15 +0100

    vl-003-Added Spring Boot base project with Junit dependencie

---
+++ vloja-backend/vloja/src/main/java/com/tqs/vloja/VlojaApplication.java
@@ -0,0 +1,13 @@
+package com.tqs.vloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlojaApplication.class, args);
+	}
+
+}
